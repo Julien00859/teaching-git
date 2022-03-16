77f4010384bafce2392ee49d44eccd5b79ee0378
Julien Castiaux <julien.castiaux@gmail.com>
2022-03-16 10:47:55 +0100

    feat(maths): multiplication primitive
    Task: 2

--- maths.py
+++ maths.py
@@ -2,8 +2,18 @@ def add(a, b):
     return a + b
 
 
+def mul(a, b):
+    c = 0
+    for _ in range(b):
+        c += a
+    return c
+
+
 if __name__ == '__main__':
     import sys
     if '--test' in sys.argv:
         assert add(0, 0) == 0
         assert add(20, 10) == 30
+        assert mul(1, 0) == 0
+        assert mul(0, 1) == 0
+        assert mul(2, 6) == 12
