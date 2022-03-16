bed79d60f4858d59fef0183cb6bf879ee53c8ea2
Julien Castiaux <julien.castiaux@gmail.com>
2022-03-16 10:42:41 +0100

    feat(maths): new substraction primitive
    Task: 1

--- maths.py
+++ maths.py
@@ -1,9 +1,15 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 
 if __name__ == '__main__':
     import sys
     if '--test' in sys.argv:
         assert add(0, 0) == 0
         assert add(20, 10) == 30
+        assert sub(0, 0) == 0
+        assert sub(1, 0) == 1
+        assert sub(0, 1) == -1
