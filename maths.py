b6f129b0f13dd504f91dceb4d54936d44453d2b8
Julien Castiaux <julien.castiaux@gmail.com>
2022-04-19 22:00:06 +0200

    feat(maths): sort the functions by name

--- maths.py
+++ maths.py
@@ -1,15 +1,6 @@
 def add(a, b):
     return a + b
 
-def sub(a, b):
-    return a - b
-
-
-def sign(n, zero=1):
-    if not n:
-        return zero
-    return n / abs(n)
-
 
 def mul(a, b):
     c = 0
@@ -18,19 +9,29 @@ def mul(a, b):
     return c
 
 
+def sign(n, zero=1):
+    if not n:
+        return zero
+    return n / abs(n)
+
+
+def sub(a, b):
+    return a - b
+
+
 if __name__ == '__main__':
     import sys
     if '--test' in sys.argv:
         assert add(0, 0) == 0
         assert add(20, 10) == 30
-        assert sub(0, 0) == 0
-        assert sub(1, 0) == 1
-        assert sub(0, 1) == -1
-        assert sign(1) == 1
-        assert sign(-1) == -1
         assert mul(1, 0) == 0
         assert mul(0, 1) == 0
         assert mul(2, 6) == 12
         assert mul(-2, 6) == -12
         assert mul(6, -2) == -12
         assert mul(-2, -6) == 12
+        assert sign(1) == 1
+        assert sign(-1) == -1
+        assert sub(0, 0) == 0
+        assert sub(1, 0) == 1
+        assert sub(0, 1) == -1
