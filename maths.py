1f6d517092b90dfe8c6ba46844d269cbaf53946c
Julien Castiaux <j.castiaux@ephec.be>
2023-02-14 21:13:45 +0100

    chore(maths): missing tests for add
    A new function was introduced in b8f7566 but the unittest were
    missings. This commit adds a way to test the code along with some tests
    for that function.
    
    To run the tests simply start `maths.py` right away instead of importing
    it. It exists with a non-zero status code and a message when a test
    break. It prints "ok" and exits with a zero status code when all tests
    passed.
    
        $ python3 maths.py  # "py maths.py" on windows
        ok
        $

--- maths.py
+++ maths.py
@@ -1,3 +1,10 @@
 def add(a, b):
     """ Compute the sum of two numbers """
     return a + b
+
+
+if __name__ == '__main__':
+    assert add(1, 2) == 3
+    assert add(1, 0) == 1
+    assert add(-2, 1) == -1
+    print("ok")
