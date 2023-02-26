2099611026cc510078b957cfc41c103d88a460ec
Julien Castiaux <j.castiaux@ephec.be>
2023-02-26 14:05:57 +0100

    chore(maths): sort functions by name
    The current order of functions follow a sense of "complexity", i.e. the
    addition is before the substraction and before the multiplication
    because it is an "easier" operation. As there are more and more
    functions added into the module, it becomes clear that it would be
    better to sort them by name to ease navigating into the source code.

--- maths.py
+++ maths.py
@@ -2,9 +2,11 @@ def add(a, b):
     """ Compute the sum of two numbers """
     return a + b
 
-def sub(a, b):
-    """ Compute the difference of two numbers """
-    return a - b
+def div(a, b):
+    """ Compute the quotient of two numbers """
+    if b == 0:
+        raise DivisionByZeroError()
+    return a / b
 
 def mul(a, b):
     """ Compute the product of two numbers """
@@ -13,22 +15,20 @@ def mul(a, b):
         m += a
     return m
 
-def div(a, b):
-    """ Compute the quotient of two numbers """
-    if b == 0:
-        raise DivisionByZeroError()
-    return a / b
+def sub(a, b):
+    """ Compute the difference of two numbers """
+    return a - b
 
 if __name__ == '__main__':
     assert add(1, 2) == 3
     assert add(1, 0) == 1
     assert add(-2, 1) == -1
-    assert sub(1, 2) == -1
-    assert sub(2, 1) == 1
-    assert sub(1, 0) == 1
+    assert div(6, 2) == 3
+    assert div(3, -1) == -3
     assert mul(3, 2) == 6
     assert mul(3, 0) == 0
     assert mul(-1, 5) == -5
-    assert div(6, 2) == 3
-    assert div(3, -1) == -3
+    assert sub(1, 2) == -1
+    assert sub(2, 1) == 1
+    assert sub(1, 0) == 1
     print("ok")
