0ed13108c525a0de52c06257fbf7a6e76cc05aae
Julien Castiaux <j.castiaux@ephec.be>
2023-02-26 13:48:28 +0100

    feat(maths): new common arithmetic operators (-*/)
    We include three new functions for the substraction[^1],
    multiplication[^2] and division[^3].
    
    [^1]: https://en.wikipedia.org/wiki/Subtraction
    [^2]: https://en.wikipedia.org/wiki/Multiplication
    [^3]: https://en.wikipedia.org/wiki/Division_(mathematics)

--- maths.py
+++ maths.py
@@ -2,9 +2,33 @@ def add(a, b):
     """ Compute the sum of two numbers """
     return a + b
 
+def sub(a, b):
+    """ Compute the difference of two numbers """
+    return a - b
+
+def mul(a, b):
+    """ Compute the product of two numbers """
+    m = 0
+    for _ in range(b):
+        m += a
+    return m
+
+def div(a, b):
+    """ Compute the quotient of two numbers """
+    if b == 0:
+        raise DivisionByZeroError()
+    return a / b
 
 if __name__ == '__main__':
     assert add(1, 2) == 3
     assert add(1, 0) == 1
     assert add(-2, 1) == -1
+    assert sub(1, 2) == -1
+    assert sub(2, 1) == 1
+    assert sub(1, 0) == 1
+    assert mul(3, 2) == 6
+    assert mul(3, 0) == 0
+    assert mul(-1, 5) == -5
+    assert div(6, 2) == 3
+    assert div(3, -1) == -3
     print("ok")
